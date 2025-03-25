957b7d65738091e89efb6a0006448b4d9a38c317
sarwenda <142522186+saputribosen@users.noreply.github.com>
2025-03-25 23:00:28 +0700

    Update sslmar.sh

--- sslmar.sh
+++ sslmar.sh
@@ -41,11 +41,11 @@ wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/saputr
 #wget -q -O tesidom.sh https://raw.githubusercontent.com/Agunxzzz/MarXray/main/tesidom.sh && chmod +x tesidom.sh && ./tesidom.sh
 sleep 1
 rm -r /etc/nginx/nginx.conf
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Agunxzzz/Mina-Xray-SSH/main/conf/nginx.conf"
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/saputribosen/Xray-SSH/main/conf/nginx.conf"
 service nginx restart
 cd
 ##wget -qO- https://github.com/Gozargah/Marzban-examples/releases/latest/download/multi-port.tar.gz | tar xz --xform 's/multi-port/marzban/' && cd marzban
-wget -qO- https://github.com/Agunxzzz/MarXray/raw/main/multi-port.tar.gz | tar xz --xform 's/multi-port/marzban/' && cd marzban
+wget -qO- https://github.com/saputribosen/mazray/raw/main/multi-port.tar.gz | tar xz --xform 's/multi-port/marzban/' && cd marzban
 rm -r xray_config.json
 wget -q -O /root/marzban/xray_config.json "https://raw.githubusercontent.com/saputribosen/mazray/main/xray_config.json"
 docker compose up -d
